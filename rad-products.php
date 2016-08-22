28d3abca33bd52fe20cde89c431490b2d8919f29
Melissa Cabral <melissacabral@gmail.com>
2016-08-22 09:23:22 -0700

    after rad slider :movie_camera:

--- rad-products.php
+++ rad-products.php
@@ -28,4 +28,39 @@ function rad_products_cpt(){
  			'not_found'		=> 'No Products Found',
 		),
 	) );
+
+	register_taxonomy( 'brand', 'product', array(
+		'hierarchical' 		=> true,  //behave like categories - checkbox interface
+		'show_admin_column' => true,
+		'label'				=> 'Brands',  //human-friendly admin nav label
+		'labels' 			=> array(
+			'add_new_item' 		=> 'Add New Brand',
+			'search_items'		=> 'Search Brands',
+			'not_found' 		=> 'No Brands Found',
+		),
+
+	) );
+
+	//non-hierarchical taxo, like tags
+	register_taxonomy( 'feature', 'product', array(
+ 		'show_admin_column' => true,
+		'label'				=> 'Features',  //human-friendly admin nav label
+		'labels' 			=> array(
+			'add_new_item' 		=> 'Add New Feature',
+			'search_items'		=> 'Search Features',
+			'not_found' 		=> 'No Features Found',
+			'popular_items'		=> 'Popular Features',
+			'edit_item'			=> 'Edit Feature',
+		),
+	) );
+}
+
+
+/**
+ * Automatically flush the permalinks when this plugin is activated
+ */
+register_activation_hook( __FILE__ , 'rad_products_flush' );
+function rad_products_flush(){
+	rad_products_cpt();
+	flush_rewrite_rules();
 }
